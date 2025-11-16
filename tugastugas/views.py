40feac67cf5e647d9a0856ecd2be641c67b47353
DikanR <dikan42724@gmail.com>
2025-11-16 10:57:34 +0800

    Tugas 2 update

--- tugastugas/views.py
+++ tugastugas/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .algorithms import algoritma_genetika as ag
+import random
 
 # def index(request):
 #     test = "ini adalah teks"
@@ -13,3 +15,28 @@ def index(request):
         'test': "ini adalah teks"
     }
     return render(request, 'tugas.html', context)
+
+def tugas2(request):
+    items = {
+        'A': {'weight': 7, 'value': 5},
+        'B': {'weight': 2, 'value': 4},
+        'C': {'weight': 1, 'value': 7},
+        'D': {'weight': 9, 'value': 2},
+    }
+    capacity = 15
+    generations = 8
+
+    random.seed(42)  # replikasi konsisten menggunakan seed ini global menyeluruh
+    results, best_result = ag.genetic_algorithm(pop_size=8, generations=generations, crossover_rate=0.8, items=items, capacity=capacity, mutation_rate=0.1)
+
+    # print(results)
+    # print(best_result)
+
+    context = {
+        'generations': generations,
+        'items': items,
+        'capacity': capacity,
+        'results': results,
+        'best_result': best_result
+    }
+    return render(request, 'tugas2.html', context)
